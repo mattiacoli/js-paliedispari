3160d1986dc7eb045077d1a4888a0020a0a65af1
mattiacoli <mattia.coli@aol.com>
2025-02-14 15:25:17 +0100

    add function

--- palindormo/main.js
+++ palindormo/main.js
@@ -6,11 +6,11 @@
 // chiedere all'utente di inserire una parola - userWord = prompt(...)
 
 let userWord = 'abba';
-console.log(userWord.length);
 
 
 
-// controllare la prima meta` della parola sia uguale alla seconda - loop for
+
+/* // controllare la prima meta` della parola sia uguale alla seconda - loop for
 for (let i = 0; i < userWord.length / 2; i++) {
   const firstHalfWord = userWord[i];
 
@@ -21,4 +21,27 @@ for (let i = 0; i < userWord.length / 2; i++) {
     
   }
   
+} */
+
+// Creare una funzione
+
+function isPalidrome(word) {
+  
+  const wordLenght = word.length
+
+  for (let i = 0; i < wordLenght / 2; i++) {
+    const firstHalfWord = word[i];
+
+    if (firstHalfWord !== word[wordLenght - 1 - i]) {
+      return 'It is not a palindrome';
+    }
+    return 'It is a palindrome';
+  }
 }
+
+const result = isPalidrome(userWord)
+
+
+console.log(result);
+
+
